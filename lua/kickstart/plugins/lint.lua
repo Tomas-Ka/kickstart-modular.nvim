68ecc210e390fedc1d56bb0883c64edd1d51002d
Tomas-Ka <tomas.kamsater@gmail.com>
2024-10-26 01:26:50 +0200

    fixed duplicate mason install

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -38,7 +38,7 @@ return {
       lint.linters_by_ft['ruby'] = nil
       lint.linters_by_ft['terraform'] = nil
       lint.linters_by_ft['text'] = nil
-      lint.linters_by_ft['python'] = { 'pflake8' }
+      lint.linters_by_ft['python'] = { 'flake8' }
 
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
