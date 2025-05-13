8fc71b299f5999a258717941f9f14d2670a2ae51
Tomas-Ka <tomas.kamsater@gmail.com>
2025-05-13 23:56:32 +0200

    Improve on move selected lines keybind

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -46,8 +46,17 @@ vim.keymap.set('n', '<leader>x', '<cmd>bd<CR>', { desc = 'Close current buffer' 
 vim.keymap.set('n', '<leader>sm', '<cmd>lua require"telescope".extensions.metals.commands()<CR>', { desc = 'Open metals run dialogue' })
 
 -- Move selected lines up and down
-vim.keymap.set('v', 'K', ":move '<-2<CR> gv=gv")
-vim.keymap.set('v', 'J', ":move '>+1<CR> gv=gv")
+-- vim.keymap.set('v', 'K', ":move '<-2<CR> gv=gv")
+-- vim.keymap.set('v', 'J', ":move '>+1<CR> gv=gv")
+vim.keymap.set('v', 'K', function()
+  local lines_to_move = (vim.v.count == 0) and 2 or (vim.v.count + 1)
+  return ":move '<-" .. lines_to_move .. '<CR> gv=gv'
+end, { expr = true })
+
+vim.keymap.set('v', 'J', function()
+  local lines_to_move = (vim.v.count == 0) and 1 or vim.v.count
+  return ":move '>+" .. lines_to_move .. '<CR> gv=gv'
+end, { expr = true })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
