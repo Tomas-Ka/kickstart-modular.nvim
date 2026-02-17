3ebaa7fa5365c326f7c4b0129174e7ceca2edf7b
Tomas-Ka <tomas.kamsater@gmail.com>
2026-02-17 03:12:18 +0100

    remove superfluous version check

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -128,11 +128,12 @@ return {
           ---@param bufnr? integer some lsp support methods only in specific files
           ---@return boolean
           local function client_supports_method(client, method, bufnr)
-            if vim.fn.has 'nvim-0.11' == 1 then
-              return client:supports_method(method, bufnr)
-            else
-              return client.supports_method(method, { bufnr = bufnr })
-            end
+            return client:supports_method(method, bufnr)
+            -- if vim.fn.has 'nvim-0.11' == 1 then
+            --   return client:supports_method(method, bufnr)
+            -- else
+            --   return client.supports_method(method, { bufnr = bufnr })
+            -- end
           end
 
           map('K', vim.lsp.buf.hover, 'Pee[k] at symbol info', 'n')
