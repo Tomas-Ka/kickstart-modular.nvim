f9f72868abfba9bcae09cd8482351093234162e8
Tomas-Ka <tomas.kamsater@gmail.com>
2026-02-17 01:40:56 +0100

    fix tailwind

--- lua/custom/plugins/tabset.lua
+++ lua/custom/plugins/tabset.lua
@@ -12,7 +12,7 @@ return {
           expandtab = false,
         },
         {
-          filetypes = { 'lua', 'java', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'json', 'yaml' },
+          filetypes = { 'lua', 'java', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'json', 'yaml', 'svelte' },
           config = {
             tabwidth = 2,
           },
